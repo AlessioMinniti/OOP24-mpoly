8f2be5353cb778307c842bed030719bc7d918b58
AlessioMinniti <alessio.minniti@studio.unibo.it>
2026-04-27 11:35:54 +0200

    final fix

--- settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    // Automatically downloads the correct java version to run the static analyzers
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-}
-
-rootProject.name = "mpoly"
